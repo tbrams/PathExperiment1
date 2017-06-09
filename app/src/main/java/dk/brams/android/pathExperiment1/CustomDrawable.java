ce43a56697f012ea4859a52badc191eef61d7690
tbrams <torben@brams.dk>
2017-06-09 12:55:16 +0700

    Moved initialisation stuff from onDraw to the constructor

--- app/src/main/java/dk/brams/android/pathExperiment1/CustomDrawable.java
+++ app/src/main/java/dk/brams/android/pathExperiment1/CustomDrawable.java
@@ -10,17 +10,28 @@ import android.view.View;
 
 public class CustomDrawable extends View {
 
-    Point[] myPath = {new Point(100, 100),
+    Point[] myPoint = {new Point(100, 100),
             new Point(200, 200),
             new Point(200, 500),
             new Point(400, 500),
             new Point(400, 200)
     };
 
+    Paint myPaint = new Paint();
+    Path myPath = new Path();
+
 
     public CustomDrawable(Context context) {
         super(context);
-        // TODO Auto-generated constructor stub
+
+        myPaint.setColor(Color.RED);
+        myPaint.setStrokeWidth(8);
+        myPaint.setStyle(Paint.Style.STROKE);
+
+        myPath.moveTo(myPoint[0].x, myPoint[0].y);
+        for (int i = 1; i < myPoint.length; i++) {
+            myPath.lineTo(myPoint[i].x, myPoint[i].y);
+        }
     }
 
 
@@ -28,17 +39,6 @@ public class CustomDrawable extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        Paint paint = new Paint();
-        paint.setColor(Color.RED);
-        paint.setStrokeWidth(8);
-        paint.setStyle(Paint.Style.STROKE);
-        Path path = new Path();
-
-        path.moveTo(myPath[0].x, myPath[0].y);
-        for (int i = 1; i < myPath.length; i++) {
-            path.lineTo(myPath[i].x, myPath[i].y);
-        }
-
-        canvas.drawPath(path, paint);
+        canvas.drawPath(myPath, myPaint);
     }
 }
